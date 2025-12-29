4ac93e60634172b2458e429c953fb963b0782d7e
EdInTheNet <eduardlobes@gmail.com>
2025-12-29 21:56:10 +0100

    first draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { auftragsAnsicht } from './shared/interfaces';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'zeiterfassung';
+export class AppComponent implements OnInit {
+
+  //** HAUPTANSICHT */
+
+  auftragsId = signal<number>(0); // zuständig für die interne id verteilung pro Auftrag ( nicht sichtbar für den user)
+  auftragsNummer = signal<string>('');// BA Nummer zum Beispiel oderr interne Auftragsnummer
+
+  //** NEBENANSICHT FÜR EINEN ODERER MEHRERE BESTANDTEILE EINSE AUFTRAGES Bsp. welche Teile geneau bearbeitet werden sollen */
+
+  auftragBezeichnung = signal<string>(''); // Bsp. Montage, Zuschneiden, Sägen oder Kleben
+  auftragsObjekt = signal<string>('');// Das Endergebeniss des Auftrages Bsp. ein Schrank 100 X 400
+  auftragsMaterial = signal<string>('');// das Werkstück oder das material um das es sich handelt beim jetzigen Arbeitsschritt
+
+  ngOnInit() {
+    this.setData();
+    this.erstelleAuftrag();
+  };
+
+  setData(): void {
+    const testDaten: any[] = [
+      { AuftragsNummer: '902.25.2345.00.01', AuftragsBezeichnung: 'Montage Einheit', AuftragsObjekt: 'C Basic', AuftraggsMaterial: 'HPL' },
+    ];
+    const d = testDaten[0];
+    this.auftragsNummer.set(d.AuftragsNummer);
+    this.auftragBezeichnung.set(d.AuftragsBezeichnung);
+    this.auftragsObjekt.set(d.AuftragsObjekt);
+    this.auftragsMaterial.set(d.AuftraggsMaterial);
+  };
+
+  callImageUrl() {
+    //TODO Stock Bilder für alle Material und Auftrags Arten suchen und einfügen und dann abrufen
+  }
+
+  routerToZeiterfassung() {
+    // einen router zur Zeiterfassung einbauen mit dem der user direkt seine Auftragszeit messen kann
+    return null;
+  }
+
+  routerToMagazienApp() {
+    // Rout zur Magayien App um das Material zu bessorgen 
+    return null;
+  }
+
+  routToEdrawing() {
+    //rout zum Edrawing bzw window.open()
+    return null;
+  }
+
+  erstelleAuftrag(): void {
+    const newAuftrag: auftragsAnsicht = {
+      AuftragsNummer: this.auftragsNummer(),
+      AuftragsBezeichnung: this.auftragBezeichnung(),
+      AuftragsObjekt: this.auftragsObjekt(),
+      AuftraggsMaterial: this.auftragsMaterial(),
+    };
+    console.log(newAuftrag);
+  };
+
 }
---
+++ src/app/shared/interfaces.ts
@@ -0,0 +1,6 @@
+export interface auftragsAnsicht {
+    AuftragsNummer: string;
+    AuftragsBezeichnung: string;
+    AuftragsObjekt: string;
+    AuftraggsMaterial: string;
+}
